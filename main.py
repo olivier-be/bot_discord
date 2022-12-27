c276278ef0adb8b90db498d546afe59898ebae32
Olivier Bensemhoun <olivier.bensemhoun@epita.fr>
2022-12-27 01:13:36 +0100

    discord_bot v1.4
    respect syntax rules in python

--- main.py
+++ main.py
@@ -1,89 +1,87 @@
-
 import discord
 import random
 import game
 from discord.ext import commands
-import os
 import openai
 
 import private_key
 
-openai.organization = private_key.openai_org #openai key with org-
-openai.api_key = private_key.openai_api_key #openai api key
-
+openai.organization = private_key.openai_org  # openai key with org-
+openai.api_key = private_key.openai_api_key  # openai api key
 
 client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
-objects =game.objects
+objects = game.objects
+
+
 @client.event
 async def on_ready():
     print('Ready!')
 
     await client.change_presence(activity=discord.Game("I will spread toxicity throughout this server!"))
 
-games=False
-channel_game= ""
-find=""
-@client.event
 
+games = False
+channel_game = ""
+find = ""
+
+
+@client.event
 async def on_message(message):
-    global objects,channel_game,find,games
-    username=str(message.author).split('#')[0]
-    message_content=str (message.content)
-    channel=str(message.channel.name)
+    global objects, channel_game, find, games
+    username = str(message.author).split('#')[0]
+    message_content = str(message.content)
+    channel = str(message.channel.name)
     print(f'{username}: {message_content} {channel}')
     if message.author == client.user:
         return
-        await message.channel.send(response)
-    elif ("feur") in message_content and not(games):
-        await message.channel.send("me ta gueule") # reponse in french to french bot feur
-    elif ( "bot") in message.content :
+    elif "feur" in message_content and not games:
+        await message.channel.send("me ta gueule")  # response in French to bot feur
+    elif "bot" in message.content:
         e = discord.Embed()
         e.set_thumbnail(url="https://media.tenor.com/8XNZFtwJxscAAAAC/reverse-card-uno.gif")
         await message.channel.send(embed=e)
-    elif ("!openai_image") in message_content:
+    elif "!openai_image" in message_content:
         response = openai.Image.create(
-            prompt=message_content[13 : len(message_content)],
+            prompt=message_content[13: len(message_content)],
             n=1,
             size="1024x1024"
         )
         await message.channel.send(response['data'][0]['url'])
-    elif ("!openai") in message_content:
-        str1=message_content[7 : len(message_content)]
-        messager= openai.Completion.create(model="text-davinci-003", prompt=str1, temperature=0, max_tokens=500)
-        print (messager['choices'][0]['text'])
-        await message.channel.send(str(messager['choices'][0]['text']))
-    elif ("toxic") in message_content:
+    elif "!openai" in message_content:
+        str1 = message_content[7: len(message_content)]
+        messages = openai.Completion.create(model="text-davinci-003", prompt=str1, temperature=0, max_tokens=500)
+        print(messages['choices'][0]['text'])
+        await message.channel.send(str(messages['choices'][0]['text']))
+    elif "toxic" in message_content:
         await message.channel.send("I am here")
-    elif ("!version") in message_content:
+    elif "!version" in message_content:
         await message.channel.send("https://github.com/olivier-be/bot_discord/")
         await message.channel.send("V1.2 fair bot")
-    elif ("windows") in message_content:
+    elif "windows" in message_content:
         await message.channel.send("linux > ")
-    elif ("!game") in message_content:
+    elif "!game" in message_content:
         await game_start(message, channel)
-    elif  games and channel_game==channel:
-        if message_content=="end":
-            games=False
+    elif games and channel_game == channel:
+        if message_content == "end":
+            games = False
             await message.channel.send("You can make better next time ")
-        else :
-            if message_content== find:
+        else:
+            if message_content == find:
 
                 games = False
                 await message.channel.send("gg")
             else:
-                messager = openai.Completion.create(model="text-davinci-003",
-                                                    prompt="give an hint without give responce for find  = "+find+ " with last word give "+message_content+" give information about if is close to find the word",
+                messages = openai.Completion.create(model="text-davinci-003",
+                                                    prompt="give an hint without give responce for find  = " + find + "with last word give " + message_content + " give information about if is close to find the word",
                                                     temperature=0, max_tokens=50)
-                await message.channel.send(messager['choices'][0]['text'])
-    elif ("!stopgame") in message_content:
-        games=False
-
-
+                await message.channel.send(messages['choices'][0]['text'])
+    elif "!stopgame" in message_content:
+        games = False
 
 
 @client.command()
-async def game_start(message,channel):
-    global games,channel_game,find
+async def game_start(message, channel):
+    global games, channel_game, find
     if games:
         if channel_game != channel:
             await message.channel.send("you need to close your old game in the channel")
@@ -97,4 +95,4 @@ async def game_start(message,channel):
         await message.channel.send("What am i thinking ?")
 
 
-client.run(private_key.discord_key) #discord api key
+client.run(private_key.discord_key)  # discord api key
