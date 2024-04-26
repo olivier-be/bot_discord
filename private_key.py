029490bcffb95ff8ba7e911866da1346c890b799
olivier-be <61296476+olivier-be@users.noreply.github.com>
2024-04-26 16:32:50 +0200

    Update private_key.py
    remove 'use'

--- private_key.py
+++ private_key.py
@@ -2,4 +2,4 @@ from os import getcwd
 openai_api_key = ""
 openai_org = "http://localhost:8080/v1" #for llama.cpp
 discord_key = ""
-path = use getcwd() #for default path
+path = getcwd() #for default path
