87a22b9178f9206a5e3c1e08fc15aee848e74067
Olivier Bensemhoun <olivier.bensemhoun@epita.fr>
2025-06-02 18:42:16 +0200

    fix: if and create sub function

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ url = 'https://api.github.com/repos/olivier-be/bot_discord/tags'
 response = requests.get(url)
 tag = response.json()
 
+print(config)
 if  config["version"]["version"] == tag[0]['name']:
     print("last update install")
 else:
@@ -67,26 +68,6 @@ async def on_message(message): # look at all the messages
 
     if message.author == client.user:
         return
-
-    elif " bot " in message.content:
-        e = discord.Embed()
-        e.set_thumbnail(url="https://media.tenor.com/8XNZFtwJxscAAAAC/reverse-card-uno.gif")
-        await message.channel.send(embed=e)
-    elif i < len(games) and games[i] != None and games[i][0] == channel and message_content != "!end" :
-        if message_content == games[i][1]:
-
-            games[i]=None
-            await message.channel.send("gg "+message.author.mention)
-        else:
-            messages = client_llama.chat.completions.create(
-                    model="gpt-3.5-turbo",
-                    messages=[
-                    {"role": "system", "content": "Short responce for the game .The game is to make the word guess :" + games[i][1] + ". In the answer the last answer give is : " + message_content + ". Never say " + games[i][1]},
-                            {"role": "user", "content":"give me a hint"}
-                            ]
-                        )
-
-            await message.channel.send(messages.choices[0].message.content)
     else:
         await client.process_commands(message)
 
@@ -102,59 +83,11 @@ async def spam(ctx, amount: int, size: int,*, message):
     else:
         await ctx.send(message)
 
-
-@client.command()
-async def Game(message): # start game
-    channel=message.channel
-    global games
-    isfind=False
-    i=0
-    while i<len(games) and games[i]!=None and games[i][0] != channel :
-        i+=1
-    if i == len(games):
-        find = objects[int(random.uniform(0, 374))]
-        games.append((str(channel), find))
-        print(find)
-        await message.channel.send("What am i thinking ?")
-    elif games[i]!=None and games[i][0] == channel:
-        await message.channel.send("one game already run")
-    else:
-        find = objects[int(random.uniform(0, 374))]
-        games[i]=(str(channel), find)
-        print(find)
-        await message.channel.send("What am i thinking ?")
-
 @client.command()
 async def version(ctx):
     await ctx.channel.send("https://github.com/olivier-be/bot_discord/")
     await ctx.channel.send("{} fair bot".format(config["version"]["version"]))
 
-@client.command()
-async def end(ctx):
-    global games
-    channel=ctx.channel
-    i=0
-    while i<len(games) and (games[i]==None or games[i][0] != str(channel)) :
-        i+=1
-    if i == len(games):
-        await ctx.channel.send("any game is running ont this channel")
-    else:
-        games[i]=None
-        await ctx.channel.send("You can make better next time "+ ctx.author.mention)
-
-
-@client.command()
-async def stopgame(ctx):
-    global games
-    channel=ctx.channel
-    i=0
-    while i < len(games) and games[i] != None and games[i][0] != channel:
-        i += 1
-    if i == len(games):
-        await ctx.channel.send("any game is running ont this channel")
-    else:
-        games[i] = None
-        await ctx.channel.send("You close the running game ")
 
 @client.command()
 async def clear_message(ctx,nb:int):
@@ -266,24 +199,9 @@ async def quote(message,*,message_content:str): #write word on image
 
 
 def dockerconfig(s ,version,is_whitelist,is_crack):
-    data = {
-       'services': {
-            'minecraft-server':{
-                'image':'itzg/minecraft-server',
-                'tty':'true',
-                'stdin_open':'true',
-                'ports':['25565:25565'],
-                'environment':
-                    {
-                        'VERSION':version,
-                        'EULA':"TRUE",
-                        'ENABLE_COMMAND_BLOCK':"TRUE",
-                        'MEMORY': "10G"
-                    },
-                'volumes':[ './data:/data'],
-                    },
-                },
-        }
+    data = {};
+    with open(path + "/default-mincraft.yml") as stream:
+        data = yaml.safe_load(stream)
     if is_whitelist == "true":
         data['services']['minecraft-server']['environment']['WHITELIST'] = "TRUE"
     if is_crack == "true":
@@ -302,6 +220,32 @@ async def minecraft_setup(message,is_whitelist,is_crack,*,version:str):
         await message.channel.send("data for server setup")
     else:
         await message.channel.send("demand to bot admin for setup the server")
+
+
+def dockerconfig_mod(s ,version,is_whitelist,is_crack,):
+    data = {};
+    with open(path + "/default-mincraft.yml") as stream:
+        data = yaml.safe_load(stream)
+    if is_whitelist == "true":
+        data['services']['minecraft-server']['environment']['WHITELIST'] = "TRUE"
+    if is_crack == "true":
+        data['services']['minecraft-server']['environment']['ONLINE_MODE'] = "FALSE"
+    with open(s + '/docker-compose.yaml', 'w') as file:
+         yaml.dump(data, file)
+
+
+
+
+@client.command()
+async def minecraft_setup_mod(message,is_whitelist,is_crack,*,version:str): 
+    if message.author.id in private_key.admin or message.author.mention == discord.Permissions.administrator:
+        s =  private_key.path + "mincraft-" + str(message.guild.id)
+        subprocess.run(["mkdir", s])
+        dockerconfig(s,version,is_whitelist,is_crack)
+        await message.channel.send("data for server setup")
+    else:
+        await message.channel.send("demand to bot admin for setup the server")
+
 
 
 @client.command() 
@@ -474,6 +418,26 @@ def is_zip_file(file_path):
     except zipfile.BadZipFile:
        return False
 
+async def zip_file(s):
+    if is_zip_file(s + '/file.zip'):
+        await message.channel.send("download succes")
+        subprocess.run(["mkdir", s + '/data/world'])
+        subprocess.run(['mv',s + '/data/ops.json',s + "/"])
+        subprocess.run(["mkdir", s + '/temp' ])  
+        subprocess.run(['unzip','-o',
+                    s + '/file.zip','-d',s + '/temp'])
+        subprocess.run(["chmod","-R","777",s + '/data'])
+        move_files(s + '/temp', s + '/data/world')
+        subprocess.run(['mv',s + '/ops.json',s + '/data/ops.json']) 
+        await message.channel.send("extrated and move world")
+        subprocess.run(['chown','-R','opc:opc',s + '/data'])
+        subprocess.run(["rm","-rf", s + '/file.zip' ])
+        subprocess.run(["rm","-rf", s + '/temp' ])
+        await message.channel.send("data for server setup")
+    else:
+        await message.channel.send("error download")
+        await message.channel.send("downloaded file is not a zip file for minecraft")
+
 
 @client.command()
 async def minecraft_map(message,version:str,website:str,end:str): #write word on image
@@ -499,26 +463,7 @@ async def minecraft_map(message,version:str,website:str,end:str): #write word on
 
         subprocess.run(['wget','-t','20','https://www.' + website +"/"+ end
                        ,'-O',s + '/file.zip'])
-        
-        if is_zip_file(s + '/file.zip'):
-            await message.channel.send("download succes")
-            subprocess.run(["mkdir", s + '/data/world'])
-            subprocess.run(['mv',s + '/data/ops.json',s + "/"])
-            subprocess.run(["mkdir", s + '/temp' ])  
-            subprocess.run(['unzip','-o',
-                    s + '/file.zip','-d',s + '/temp'])
-            subprocess.run(["chmod","-R","777",s + '/data'])
-            move_files(s + '/temp', s + '/data/world')
-            subprocess.run(['mv',s + '/ops.json',s + '/data/ops.json']) 
-            await message.channel.send("extrated and move world")
-            subprocess.run(['chown','-R','opc:opc',s + '/data'])
-            subprocess.run(["rm","-rf", s + '/file.zip' ])
-            subprocess.run(["rm","-rf", s + '/temp' ])
-            await message.channel.send("data for server setup")
-        else:
-            await message.channel.send("error download")
-            await message.channel.send("downloaded file is not a zip file for minecraft")
-
+        zipfile(s)
     else:
         await message.channel.send("not allowed or bad website")
 
